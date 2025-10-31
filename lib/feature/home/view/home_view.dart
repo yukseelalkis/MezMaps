c11ff31fa113b714671d7b064e2ac39fad02ee90
Yuksel Alkis <yukselalkis@Yuksel-Mac-mini.local>
2025-10-31 23:18:57 +0300

    Gen fixed

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mezmaps/feature/home/view_model/mixin/home_mixin.dart';
 import 'package:mezmaps/product/state/base/base_state.dart';
 import 'package:common/common.dart';
+import 'package:gen/gen.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -14,14 +15,16 @@ class _HomeViewState extends BaseState<HomeView> with HomeViewMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('')),
-      body: ElevatedButton(
-        onPressed: () {},
-        child: Padding(
-          padding: PaddingManager.normalPaddingSymmetricHorizontal(context),
-          child: Text('deniz kamer'),
+      appBar: AppBar(
+        title: SizedBox(
+          height: 100, // Yüksekliği azaltır, 30-40 genelde ideal olur
+          child: Assets.images.logo.image(
+            package: 'gen',
+            fit: BoxFit.contain, // Taşmadan ölçeklensin
+          ),
         ),
       ),
+
       floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
   }
